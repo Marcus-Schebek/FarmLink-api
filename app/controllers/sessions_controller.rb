97258ede6c26ffdacc89b54002d7c9b912b53d3a
Marcus Schebek <marcusschebek@gmail.com>
2025-08-16 21:05:54 -0300

    Feat: Agora o backend retorna o id do usuário

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       # Gera o token JWT com o ID do usuário
       token = JsonWebToken.encode(user_id: user.id)
-      render json: { message: "Login realizado com sucesso", token: token }, status: :ok
+      render json: { message: "Login realizado com sucesso", token: token, user_id: user.id }, status: :ok
     else
       render json: { error: "Credenciais inválidas" }, status: :unauthorized
     end
